1b283deca302c971d941df6a1bbb46766c8ebdcc
GitHub Action <action@github.com>
2022-06-19 18:59:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/stevenlmcgraw/intellijcodegenerator2/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.stevenlmcgraw.intellijcodegenerator2.services
+
+import com.github.stevenlmcgraw.intellijcodegenerator2.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/stevenlmcgraw/intellijcodegenerator2/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stevenlmcgraw/intellijcodegenerator2/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stevenlmcgraw.intellijcodegenerator2.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stevenlmcgraw.intellijcodegenerator2.MyBundle
 
 class MyProjectService(project: Project) {
 
